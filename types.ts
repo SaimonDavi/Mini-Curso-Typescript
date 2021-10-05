85afc40bd8e6fc23828fd6060ae04d46b6855791
Saimon Davi <saimon.dsfg@gmail.com>
2021-10-05 11:40:19 -0300

    Adicionando um arquivo de referência de tipos do Typescript

---
+++ types.ts
@@ -0,0 +1,55 @@
+// boolean (true/false)
+let isOpen: boolean
+isOpen = true
+
+// string ('foo', "foo", `foo`)
+let message: string
+message: `foo ${isOpen}`
+
+// number (int, float, hex, binary)
+let total: number
+total = 0xff000
+
+// array (type[])
+let items: string[]
+items = ["foo", "bar"]
+
+let values: Array<number>
+values = [1, 2, 3]
+
+// tuple
+let title: [number, string, boolean]
+title = [1, 'foo', true]
+
+// enum
+enum Colors{
+  white = '#fff',
+  black = '#000'
+}
+
+// any (qualquer tipo)
+let coisa: any
+coisa = true
+coisa = 1
+coisa = "foo"
+coisa = [1]
+
+// void (retorno vazio)
+function logger(): void {
+  console.log('foo')
+}
+
+// null | undefined
+type bla = string | undefined
+
+// never 
+function error(): never {
+  throw new Error("error");
+}
+
+// object
+let cart: object
+
+cart = {
+  key: "foo"
+}
